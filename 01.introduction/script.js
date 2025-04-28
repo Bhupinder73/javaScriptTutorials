b31f71d0b2f38e70f240d2fdd1e5f8f2af898a6c
Bhupinderpreet Singh <bhupinderpreetsingh16694@gmail.com>
2025-04-28 18:55:33 +0530

    introDone

---
+++ 01.introduction/script.js
@@ -0,0 +1,6 @@
+//first function.
+function myFunction(){
+    console.log("btn clicked");
+    document.getElementById('firstPara').innerText = "Content was changed by JavaScript";
+    document.getElementsByTagName('h1')[0].style.color = 'red';
+}
